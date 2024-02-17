472f1e7b33f32a3832593967ea5e2566ef5447e2
0kan1 <test467876@gmail.com>
2024-02-17 21:58:06 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,20 +1,21 @@
 var pathName = window.location.pathname;
-var id = pathName.split('/')[2];
+        var id = pathName.split('/')[2];
 
-if (pathName.startsWith("/m/") || pathName.startsWith("/i/")) {
-    fetch("https://robotting.pythonanywhere.com" + pathName, {
-        method: "GET",
-        mode: "cors",
-        credentials: "include",  // Credentials ekleyin
-        headers: {
-            "Content-Type": "application/json"
+        if (pathName.startsWith("/m/") || pathName.startsWith("/i/")) {
+            fetch("https://robotting.pythonanywhere.com" + pathName, {
+                method: "GET",
+                mode: "cors",
+                credentials: "include",  // Credentials ekleyin
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('loading-container').style.display = 'none'; // Yüklenme ekranını gizle
+                document.body.innerHTML += data;
+            })
+            .catch(error => console.error(error));
+        } else {
+            console.error("Geçersiz URL path name");
         }
-    })
-    .then(response => response.text())
-    .then(data => {
-        document.body.innerHTML += data;
-    })
-    .catch(error => console.error(error));
-} else {
-    console.error("Geçersiz URL path name");
-}
