39c5e3b35b5891200d6639e7b69d664fc911a181
0kan1 <test467876@gmail.com>
2024-02-17 14:09:34 +0300

    Update home.js

--- static/home.js
+++ static/home.js
@@ -1,11 +1,27 @@
+var securityCookieValue = getSecurityCookieValue();
 
-fetch("https://robotting.pythonanywhere.com/home")
-.then(response => {
-    return response.text();
+fetch("https://robotting.pythonanywhere.com/home", {
+    method: "GET",
+    headers: {
+        "Cookie": "security_cookie=" + securityCookieValue
+    }
 })
+.then(response => response.text())
 .then(data => {
     document.body.innerHTML = data;
 })
 .catch(error => {
     console.error("Fetch error:", error);
 });
+function getSecurityCookieValue() {
+    var name = "security_cookie=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i].trim();
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
