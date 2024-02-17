ea92d54d0809f1ac271b5b5ba5d8415cc7cb6544
0kan1 <test467876@gmail.com>
2024-02-17 13:45:23 +0300

    main

---
+++ static/auth.js
@@ -0,0 +1,20 @@
+
+    window.onload = function() {
+        var urlParams = new URLSearchParams(window.location.search);
+        var token = urlParams.get('token');
+
+        if (token) {
+            setSecurityCookie(token);
+            window.location.href = '/home';
+        }
+    };
+
+    function setSecurityCookie(data) {
+        var securityCookieName = "security_cookie";
+        var securityCookieValue = data;
+        var expirationDays = 7;
+        var expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + expirationDays);
+        var cookieString = securityCookieName + "=" + securityCookieValue + "; expires=" + expirationDate.toUTCString() + "; path=/";
+        document.cookie = cookieString;
+    }
