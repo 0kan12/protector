6f410c3497b861db8c166b0dce05c11b73e3f8d2
0kan1 <test467876@gmail.com>
2024-02-17 12:28:21 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 var pathName = window.location.pathname;
 var id = pathName.split('/')[2];
-
 if (pathName.startsWith("/magaza/")) {
     fetch("https://robotting.pythonanywhere.com/magaza/" + id)
         .then(response => response.text())
