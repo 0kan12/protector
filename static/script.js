09b7d3501a2952a6d2d0e111ddf439cd551e3b81
0kan1 <test467876@gmail.com>
2024-02-17 12:26:48 +0300

    main

---
+++ static/script.js
@@ -0,0 +1,20 @@
+var pathName = window.location.pathname;
+var id = pathName.split('/')[2];
+
+if (pathName.startsWith("/magaza/")) {
+    fetch("https://robotting.pythonanywhere.com/magaza/" + id)
+        .then(response => response.text())
+        .then(data => {
+            document.body.innerHTML += data;
+        })
+        .catch(error => console.error(error));
+} else if (pathName.startsWith("/ilan/")) {
+    fetch("https://robotting.pythonanywhere.com/ilan/" + id)
+        .then(response => response.text())
+        .then(data => {
+            document.body.innerHTML += data;
+        })
+        .catch(error => console.error(error));
+} else {
+    console.error("Geçersiz URL path name");
+}
