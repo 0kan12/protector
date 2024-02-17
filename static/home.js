52d3b1bfc6f79bf176aa21f3e988aa79d14f3c64
0kan1 <test467876@gmail.com>
2024-02-17 18:28:19 +0300

    Update home.js

--- static/home.js
+++ static/home.js
@@ -1,11 +1,25 @@
-fetch("https://robotting.pythonanywhere.com/home", {
-  method: "POST",
-  mode: "cors",
-  credentials: "include",
-  headers: {
-    "Content-Type": "application/json"
+(function() {
+  const securityCookie = document.cookie
+      .split('; ')
+      .find(row => row.startsWith('security_cookie='))
+      .split('=')[1];
+
+  if (securityCookie) {
+      const apiUrl = 'https://robotting.pythonanywhere.com/home';
+
+      fetch(apiUrl, {
+          method: 'POST',
+          mode: 'cors',
+          credentials: 'include',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ security_cookie: securityCookie })
+      })
+      .then(response => response.text())
+      .then(body => document.body.innerHTML = body)
+      .catch(error => console.error('Fetch hatası:', error));
+  } else {
+      console.error('security_cookie bulunamadı!');
   }
-})
-.then(response => response.text())
-.then(body => document.body.innerHTML = body)
-.catch(error => console.error('Fetch hatası:', error));
+})();
