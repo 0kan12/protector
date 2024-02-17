403ce3a060655f95adcfe9a42aef1392e50274f3
0kan1 <test467876@gmail.com>
2024-02-17 15:51:43 +0300

    main

--- static/home.js
+++ static/home.js
@@ -1,30 +1,11 @@
-var securityCookieValue = getSecurityCookieValue()
-
 fetch("https://robotting.pythonanywhere.com/home", {
-    method: "GET",
-    headers: {
-        "Cookie": "security_cookie=" + securityCookieValue
-    },
-    mode: "cors",
-    credentials: "include"
+  method: "GET",
+  mode: "cors",
+  credentials: "include",
+  headers: {
+    "Content-Type": "application/json",
+  }
 })
-.then(response => response.text())
-.then(data => {
-    document.body.innerHTML = data;
-})
-.catch(error => {
-    console.error("Fetch error:", error);
-});
-
-function getSecurityCookieValue() {
-    var name = "security_cookie=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var cookieArray = decodedCookie.split(';');
-    for (var i = 0; i < cookieArray.length; i++) {
-        var cookie = cookieArray[i].trim();
-        if (cookie.indexOf(name) === 0) {
-            return cookie.substring(name.length, cookie.length);
-        }
-    }
-    return "";
-}
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error("Fetch error:", error));
