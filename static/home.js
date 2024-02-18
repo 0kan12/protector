a6aba404c72af9e6f89377014761cade279d5269
0kan1 <test467876@gmail.com>
2024-02-18 12:55:31 +0300

    Update home.js

--- static/home.js
+++ static/home.js
@@ -1,25 +1,35 @@
 (function() {
-  const securityCookie = document.cookie
+    const securityCookie = document.cookie
       .split('; ')
-      .find(row => row.startsWith('security_cookie='))
-      .split('=')[1];
-
-  if (securityCookie) {
+      .find(row => row.startsWith('security_cookie='));
+  
+    if (securityCookie) {
+      const token = securityCookie.split('=')[1];
+  
       const apiUrl = 'https://robotting.pythonanywhere.com/home';
-
+  
       fetch(apiUrl, {
-          method: 'POST',
-          mode: 'cors',
-          credentials: 'include',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ security_cookie: securityCookie })
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
       })
-      .then(response => response.text())
-      .then(body => document.body.innerHTML = body)
-      .catch(error => console.error('Fetch hatası:', error));
-  } else {
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(body => {
+        document.body.innerHTML = body;
+      })
+      .catch(error => {
+        console.error('Fetch hatası:', error);
+      });
+    } else {
       console.error('security_cookie bulunamadı!');
-  }
-})();
+    }
+  })();
+  
