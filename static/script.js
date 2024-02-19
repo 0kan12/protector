49be9c7e21c907f42bdd2f6d24404bac8fd29d68
0kan1 <test467876@gmail.com>
2024-02-19 23:09:25 +0300

    main

--- static/script.js
+++ static/script.js
@@ -1,4 +1,43 @@
-var pathName = window.location.pathname;
+
+        // Stil öğelerini oluştur
+        var bodyStyle = document.createElement('style');
+        bodyStyle.id = 'bodyStyle';
+        bodyStyle.textContent = `
+            body {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                margin: 0;
+                background-color: #111;
+                font-family: 'Roboto', sans-serif;
+                color: white;
+            }
+        `;
+
+        var loadingStyle = document.createElement('style');
+        loadingStyle.id = 'loadingStyle';
+        loadingStyle.textContent = `
+            .loader {
+                border: 8px solid #3498db;
+                border-radius: 50%;
+                border-top: 8px solid #f1c40f;
+                width: 50px;
+                height: 50px;
+                animation: spin 1s linear infinite;
+            }
+            @keyframes spin {
+                0% {
+                    transform: rotate(0deg);
+                }
+                100% {
+                    transform: rotate(360deg);
+                }
+            }
+        `;
+        document.head.appendChild(bodyStyle);
+        document.head.appendChild(loadingStyle);
+        var pathName = window.location.pathname;
         fetch("https://robotting.pythonanywhere.com" + pathName, {
             method: "GET",
             mode: "cors",
@@ -10,14 +49,12 @@ var pathName = window.location.pathname;
         .then(response => response.text())
         .then(data => {
             var loader = document.getElementById('loader');
+            var body=document.getElementById('bodyStyle')
             if (data.startsWith("/")) {
                 window.location.href = data;
             } else {
-                var bodyStyle = document.getElementById('bodyStyle');
-                var loadingStyle = document.getElementById('loadingStyle');
-
-                bodyStyle.parentNode.removeChild(bodyStyle);
-                loadingStyle.parentNode.removeChild(loadingStyle);
+                loader.parentNode.removeChild(loader);
+                body.parentNode.removeChild(body)
                 document.body.innerHTML = data;
             }
         })
