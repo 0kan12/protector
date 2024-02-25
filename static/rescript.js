6022f72b952d8d3fd30eabd0136044c05c351632
0kan1 <test467876@gmail.com>
2024-02-25 21:09:25 +0300

    main

---
+++ static/rescript.js
@@ -0,0 +1,63 @@
+
+var bodyStyle = document.createElement('style');
+bodyStyle.id = 'bodyStyle';
+bodyStyle.textContent = `
+    body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        margin: 0;
+        background-color: #111;
+        font-family: 'Roboto', sans-serif;
+        color: white;
+    }
+`;
+
+var loadingStyle = document.createElement('style');
+loadingStyle.id = 'loadingStyle';
+loadingStyle.textContent = `
+    .loader {
+        border: 8px solid #3498db;
+        border-radius: 50%;
+        border-top: 8px solid #f1c40f;
+        width: 50px;
+        height: 50px;
+        animation: spin 1s linear infinite;
+    }
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+`;
+document.head.appendChild(bodyStyle);
+document.head.appendChild(loadingStyle);
+var pathName = window.location.pathname;
+fetch("https://robotting.pythonanywhere.com" + pathName, {
+    method: "GET",
+    mode: "cors",
+    credentials: "include",
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+.then(response => response.text())
+.then(data => {
+    var loader = document.getElementById('loader');
+    var body = document.querySelector('#bodyStyle');
+    if (data.startsWith("/")) {
+        window.location.href = data;
+    } else {
+        loader.parentNode.removeChild(loader);
+        body.parentNode.removeChild(body)
+        document.body.innerHTML = data;
+    }
+})
+.catch(error => {
+    console.error(error);
+    console.error("Invalid URL path name");
+});
